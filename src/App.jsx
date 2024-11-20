149de733e5fb0d65408384e13dd5380a159def78
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-20 22:51:33 +0530

    id Added to scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import FeatureSection from './components/FeatureSection'
-import Pricing from './components/Pricing'
-import Workflow from './components/Workflow'
-import Testimonials from './components/Testimonials'
-import Footer from './components/Footer'
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import FeatureSection from './components/FeatureSection';
+import Pricing from './components/Pricing';
+import Workflow from './components/Workflow';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <HeroSection/>
-      <FeatureSection/>
-      <Workflow/>
-      <Pricing/>
-      <Testimonials/>
-      <Footer/>
+      <Navbar />
       
-     
+      <section id="hero">
+        <HeroSection />
+      </section>
+      <section id="features">
+        <FeatureSection />
+      </section>
+      <section id="workflow">
+        <Workflow />
+      </section>
+      <section id="pricing">
+        <Pricing />
+      </section>
+      <section id="testimonials">
+        <Testimonials />
+      </section>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
